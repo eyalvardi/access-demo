774697f7f91076f0ae8f8d8b90f90d1db259fdd1
Yaniv Shnaper <yshnaper@gmail.com>
2018-04-12 20:35:49 +0300

    sidebar ui color updates (hover & click)

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -16,5 +16,59 @@ export const navigation = [
       variant: 'info',
       text: 'NEW'
     }
+  },
+  {
+    name: 'Dashboard2',
+    url: '/dashboard2',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    name: 'Dashboard3',
+    url: '/dashboard3',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    name: 'Dashboard4',
+    url: '/dashboard4',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    name: 'Dashboard5',
+    url: '/dashboard5',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    name: 'Dashboard6',
+    url: '/dashboard6',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    name: 'Dashboard7',
+    url: '/dashboard7',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
   }
 ];
